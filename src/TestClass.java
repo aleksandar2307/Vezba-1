9f9c539dca12c4272e01803d9a902a04114808de
aleksandar2307 <al.petrovski23@gmail.com>
2021-03-23 15:19:52 +0100

    adding new hello

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,9 @@
 public class TestClass {
     public static void main(String[] args) {
 
+        System.out.println("Smile and Run");
+        System.out.println("Hello Sun");
+        System.out.println("That's it");
+
     }
 }
