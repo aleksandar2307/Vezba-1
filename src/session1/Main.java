ef8139b5f01d09ad7cfb38b63cb68e162754b869
aleksandar2307 <al.petrovski23@gmail.com>
2021-03-23 14:50:22 +0100

    new change

--- src/session1/Main.java
+++ src/session1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int price = 200;
+        int price = 300;
         int discountPercentage = 5;
         float productPriceAfterDiscount = price - price * (discountPercentage / 100.0f);
         System.out.println(productPriceAfterDiscount);
